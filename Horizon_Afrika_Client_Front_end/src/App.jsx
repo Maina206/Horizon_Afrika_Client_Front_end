9b5a8e97f52aa8dd254043eae2f6385a7f2370cf
Shirleen <fswaithaka@gmail.com>
2025-02-25 16:57:44 +0300

    NAVBAR SETUP INCOMPLETE

--- Horizon_Afrika_Client_Front_end/src/App.jsx
+++ Horizon_Afrika_Client_Front_end/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from "./Components/NavBar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Heading 1</h1>
-    </div>
+    <NavBar/>
+  //   <Router>
+  //   <NavBar />
+  //   <Routes>
+  //     <Route path="/" element={<Home />} />
+  //     <Route path="/destinations" element={<Destinations />} />
+  //     <Route path="/safari-packages" element={<SafariPackages />} />
+  //     <Route path="/about" element={<About />} />
+  //     <Route path="/contact" element={<Contact />} />
+  //   </Routes>
+  // </Router>
   );
 };
 
