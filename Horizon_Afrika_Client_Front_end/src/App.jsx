c5cb5e264273e24a16c23c7e1f54cde0e2153cb7
Shirleen <fswaithaka@gmail.com>
2025-03-02 21:15:43 +0300

    HA-12

--- Horizon_Afrika_Client_Front_end/src/App.jsx
+++ Horizon_Afrika_Client_Front_end/src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Home from "./Components/Home";
 import Footer from "./Components/Footer";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 const App = () => {
   return (
     <div> 
       <Home />
-      <Footer />
-      <FontAwesomeIcon icon={faCoffee} />
-
+      <Footer />  
     </div>
   );
 };
--- Horizon_Afrika_Client_Front_end/src/Components/Footer.jsx
+++ Horizon_Afrika_Client_Front_end/src/Components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
@@ -28,19 +31,19 @@ const Footer = () => {
           <h4>Follow Us</h4>
           <p>Join the thousands of other followers on our social platforms and get our latest offers</p>
           <div className="social-links">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="mailto:info@horizonafrika.com">
-              <i className="fas fa-envelope"></i>
-            </a>
-          </div>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="mailto:info@horizonafrika.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </div>
         </div>
       </div>
     </footer>
